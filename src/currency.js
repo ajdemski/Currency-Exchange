4d3f2589e214a371846c3b9d5d6611f62e38d24e
Aaron Demski <ajdemski@frontier.com>
2023-06-30 17:07:41 -0700

    I cant seem to solve the Cross Origin Request Error

--- src/currency.js
+++ src/currency.js
@@ -2,15 +2,31 @@ export default class Currency {
   static async getExchangeRate(baseCurrency, targetCurrency) {
     const apiKey = process.env.API_KEY;
     const url = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${baseCurrency}/${targetCurrency}`;
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error('Failed To Get Exchange Rates');
-    }
-    const data = await response.json();
-    if (data.result === 'success') {
-      return data.conversion_rate;
+
+    const exchangeRatesData = sessionStorage.getItem('exchangeRates');
+    let exchangeRates = {};
+
+    if (exchangeRatesData) {
+      try {
+        exchangeRates = JSON.parse(exchangeRatesData);
+      } catch (error) {
+        throw new Error('Invalid exchange rates data');
+      }
     } else {
-      throw new Error('No Results Found');
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('Failed to fetch exchange rates');
+      }
+      const data = await response.json();
+      if (data.result !== 'success') {
+        throw new Error('No results found');
+      }
+      exchangeRates = data.conversion_rates;
+      sessionStorage.setItem('exchangeRates', JSON.stringify(exchangeRates));
     }
+    if (!exchangeRates[targetCurrency]) {
+      throw new Error('Invalid exchange rate');
+    }
+    return exchangeRates[targetCurrency];
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './css/styles.css';
+import Currency from './currency';
 
 function updateDisplay(displayText) {
   document.getElementById('display_title').textContent = displayText;
@@ -7,47 +8,12 @@ function updateDisplay(displayText) {
 const baseCurrency = 'USD';
 
 async function getExchangeRate(baseCurrency, targetCurrency) {
-  const apiKey = process.env.API_KEY;
-  const url = `https://v6.exchangerate-api.com/v6/${apiKey}/pair/${baseCurrency}/${targetCurrency}`;
-
   try {
-    let exchangeRatesData = sessionStorage.getItem('exchangeRates');
-    let exchangeRates = {};
-
-    if (exchangeRatesData) {
-      try {
-        exchangeRates = JSON.parse(exchangeRatesData);
-      } catch (error) {
-        throw new Error('Invalid exchange rates data');
-      }
-    }
-
     const amount = 55;
+    const exchangeRate = await Currency.getExchangeRate(baseCurrency, targetCurrency);
 
-    if (exchangeRates && exchangeRates[targetCurrency]) {
-      const exchangeRate = exchangeRates[targetCurrency];
-      const displayText = `${amount} ${baseCurrency} = ${exchangeRate} ${targetCurrency}`;
-      updateDisplay(displayText);
-    } else {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error('Failed');
-      }
-      const data = await response.json();
-      if (data.result === 'success') {
-        exchangeRates = data.conversion_rates;
-        sessionStorage.setItem('exchangeRates', JSON.stringify(exchangeRates));
-        const exchangeRate = exchangeRates[targetCurrency];
-        if (exchangeRate) {
-          const displayText = `${amount} ${baseCurrency} = ${exchangeRate} ${targetCurrency}`;
-          updateDisplay(displayText);
-        } else {
-          throw new Error('Invalid Exchange Rate');
-        }
-      } else {
-        throw new Error('No Results Found');
-      }
-    }
+    const displayText = `${amount} ${baseCurrency} = ${exchangeRate} ${targetCurrency}`;
+    updateDisplay(displayText);
   } catch (error) {
     console.log(error);
     updateDisplay('Error');
